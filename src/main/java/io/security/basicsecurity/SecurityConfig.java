cf8eaaf5f6cd52b51b8ad404c19d34e171159f8a
kgj960425 <kgj960425@gmail.com>
2023-03-20 09:14:44 +0900

    5) logout 처리

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -1,2 +1,71 @@
-package io.security.basicsecurity;public class SecurityConfig {
+package io.security.basicsecurity;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+        ;
+        http
+                .formLogin()
+                //.loginPage("/loginPage")
+//                .defaultSuccessUrl("/")
+//                .failureUrl("/login")
+//                .usernameParameter("userId")    //로그인 페이지에서 form안의 id 기입란의 id가 "userId"로 설정됨 html설정할때도 이 이름에 맞춰줘야 오류가 안남.
+//                .passwordParameter("passwd")    //로그인 페이지에서 form안의 password 기입란의 pwd가 "passwd"로 설정됨 html설정할때도 이 이름에 맞춰줘야 오류가 안남.
+//                .loginProcessingUrl("/login_proc")
+//                .successHandler(new AuthenticationSuccessHandler() {
+//                    @Override
+//                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+//                        System.out.println("authentication" + authentication.getName());
+//                        response.sendRedirect("/");
+//                    }
+//                })
+//                .failureHandler(new AuthenticationFailureHandler() {
+//                    @Override
+//                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+//                        System.out.println("authentication" + exception.getMessage());
+//                        response.sendRedirect("/login");
+//                    }
+//                })
+//                .permitAll()
+        ;
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler(new LogoutHandler() {
+                    @Override
+                    public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
+                        HttpSession session = request.getSession();
+                        session.invalidate();
+                    }
+                })
+                .logoutSuccessHandler(new LogoutSuccessHandler() {
+                    @Override
+                    public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+                        response.sendRedirect("login");
+                    }
+                })
+                .deleteCookies("remember-me")
+        ;
+    }
 }
--- src/main/java/io/security/basicsecurity/SecurityController.java
+++ src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,2 +1,18 @@
-package io.security.basicsecurity;public class SecurityController {
+package io.security.basicsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //@RestController = @Controller + @ResponseBody , Json형태로 객체 데이터 반환
+public class SecurityController {
+
+    @GetMapping("/")
+    public String index(){
+        return "home";
+    }
+
+    @GetMapping("loginPage")
+    public String loginPage(){
+        return "loginPage";
+    }
 }
